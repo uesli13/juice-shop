e2df203f99da7a98c608de72def24cfaebb793d5
uesli13 <165309461+uesli13@users.noreply.github.com>
2024-12-18 15:10:04 +0200

    Update code-snippet.service.ts

--- frontend/src/app/Services/code-snippet.service.ts
+++ frontend/src/app/Services/code-snippet.service.ts
@@ -32,6 +32,6 @@ export class CodeSnippetService {
   }
 
   challenges (): Observable<string[]> {
-    return this.http.get<Solved>(`${this.host}`).pipe(map((response: Solved) => response.challenges), catchError((err: Error) => { throw err }))
+    return this.http.get(`${this.host}`).pipe(map((response: any) => response.challenges), catchError((err) => { throw err }))
   }
 }
